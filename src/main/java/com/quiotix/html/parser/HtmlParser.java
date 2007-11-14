9d7d49391fbb026f1051f32d3350641123df507f
Tim Pizey <timp@paneris.org>
2007-11-14 10:58:07 +0000

    Regenerated

--- src/main/java/com/quiotix/html/parser/HtmlParser.java
+++ src/main/java/com/quiotix/html/parser/HtmlParser.java
@@ -466,7 +466,10 @@ public class HtmlParser implements HtmlParserConstants {
   private int jj_gc = 0;
 
   public HtmlParser(java.io.InputStream stream) {
-    jj_input_stream = new SimpleCharStream(stream, 1, 1);
+     this(stream, null);
+  }
+  public HtmlParser(java.io.InputStream stream, String encoding) {
+    try { jj_input_stream = new SimpleCharStream(stream, encoding, 1, 1); } catch(java.io.UnsupportedEncodingException e) { throw new RuntimeException(e); }
     token_source = new HtmlParserTokenManager(jj_input_stream);
     token = new Token();
     jj_ntk = -1;
@@ -476,7 +479,10 @@ public class HtmlParser implements HtmlParserConstants {
   }
 
   public void ReInit(java.io.InputStream stream) {
-    jj_input_stream.ReInit(stream, 1, 1);
+     ReInit(stream, null);
+  }
+  public void ReInit(java.io.InputStream stream, String encoding) {
+    try { jj_input_stream.ReInit(stream, encoding, 1, 1); } catch(java.io.UnsupportedEncodingException e) { throw new RuntimeException(e); }
     token_source.ReInit(jj_input_stream);
     token = new Token();
     jj_ntk = -1;
@@ -676,6 +682,7 @@ public class HtmlParser implements HtmlParserConstants {
   final private void jj_rescan_token() {
     jj_rescan = true;
     for (int i = 0; i < 4; i++) {
+    try {
       JJCalls p = jj_2_rtns[i];
       do {
         if (p.gen > jj_gen) {
@@ -689,6 +696,7 @@ public class HtmlParser implements HtmlParserConstants {
         }
         p = p.next;
       } while (p != null);
+      } catch(LookaheadSuccess ls) { }
     }
     jj_rescan = false;
   }
